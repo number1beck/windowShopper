7ad2804f837facbca50c64edb1fdeade7064701c
Your Nameclear <jkb6131@psu.edu>
2021-10-11 13:42:44 -0400

    repo init

---
+++ windowShopper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  windowShopper
+//
+//  Created by Beck, James on 10/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
