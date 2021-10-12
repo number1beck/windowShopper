8485e467878a6ec00e22e7b53a068e9811c1e5f3
Your Nameclear <jkb6131@psu.edu>
2021-10-11 22:49:14 -0400

    calcBtn

--- windowShopper/Controller/MainVC.swift
+++ windowShopper/Controller/MainVC.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5966339111, blue: 0.03778369725, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
-
+    
+    @objc func calculate() {
+        print("We got here")
+    }
 
 }
 
